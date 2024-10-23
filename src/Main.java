e7ed8f81ac2ba408bb1219f3b998942422699437
otaviobleich <ferreiraotavio577@gmail.com>
2024-10-23 19:23:35 -0300

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+                int numero1 = 5;
+                int numero2 = 10;
+
+                int resultado = somar(numero1, numero2);
+
+                System.out.println("A soma de " + numero1 + " e " + numero2 + " é: " + resultado);
+            }
+
+            public static int somar(int a, int b) {
+                return a + b;
+            }
+        }
